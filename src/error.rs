8085393b6f4cc935eabed3daf5bf75724903e58b
Aapuji <om.robotics16@gmail.com>
2024-10-01 08:54:17 -0400

    did stuff

--- src/error.rs
+++ src/error.rs
@@ -8,7 +8,9 @@ pub enum Error {
 
 #[derive(Debug)]
 pub enum LexError {
-    UnclosedString
+    UnclosedString,
+    UnclosedParenthesis,
+    UnclosedBracket
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
